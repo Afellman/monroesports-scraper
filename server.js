283884d6ab885be838a3b8b2cd55fc5ad14eccce
andrewfmbs <andrewf@mbshighway.com>
2021-12-11 12:27:49 -0500

    foo

--- server.js
+++ server.js
@@ -1,64 +1,122 @@
-const { createServer } = require("https");
-const { parse } = require("url");
+const https = require("https");
 const fs = require("fs");
 const dev = process.env.NODE_ENV !== "production";
-const SECURE_PORT = parseInt(process.env.PORT, 10) || (4043);
-const UNSECURE_PORT = parseInt(process.env.HTTP_PORT, 10) || (8080);
+const SECURE_PORT = parseInt(process.env.PORT, 10) || 4043;
+const UNSECURE_PORT = parseInt(process.env.HTTP_PORT, 10) || 8080;
 const http = require("http");
 const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-
-
-const httpsOptions = {
-    key: fs.readFileSync(
-        path.join(__dirname, "certs", "www.goupstrong.com.key")
-    ),
-
-    cert: fs.readFileSync(
-        path.join(__dirname, "certs", "www.goupstrong.com.crt")
-    ),
-
-    ca: fs.readFileSync(
-        path.join(__dirname, "certs", "www.goupstrong.com.ca-bundle")
-    )
-};
-
-const server = http.createServer(function (req, res) {
-        res.writeHead(302, {
-            location: "https://" + req.headers.host + req.url
-        });
-        res.end();
-});
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-createServer(httpsOptions, app)
-    .listen(SECURE_PORT, () => {
-        console.log("HTTPS Server running on port " + SECURE_PORT);
-    });
-
-
-	app.get("/api/getTeamSchedule", getSchedule);
-
+const cheerio = require("cheerio");
+const request = require("request");
 
 const getSchedule = (req, res) => {
-    const schedule = fs.readFileSync("./latestSchedule.json");
-    const scheduleJSON = JSON.parse(schedule);
-    const lastGetDate = new Date(scheduleJSON.lastGet);
-    const today = new Date();
+    let currentSchedule = {};
+    let needsRefresh = true;
 
-    let currentSchedule = scheduleJSON.schedule;
+    const schedulePath = path.join(__dirname, "latestSchedule.json");
 
-    if (lastGetDate < today) {
-        fetch("www.monroesportscenter.com/go-up-strong-fall")
-            .then((res) => res.text()
-            .then((res) => {});
+    try {
+        const schedule = fs.readFileSync(schedulePath);
+        const scheduleJSON = JSON.parse(schedule);
+        needsRefresh = new Date(scheduleJSON.lastGet) < new Date();
+        currentSchedule = scheduleJSON.schedule;
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (needsRefresh) {
+        request(
+            "http://www.monroesportscenter.com/go-up-strong-fall",
+            (err, monroeRes, body) => {
+                const parsedSchedule = parseSchedule(body);
+                const saveObj = {
+                    lastGet: new Date().toISOString(),
+                    schedule: parsedSchedule
+                };
+                // fs.writeFile(schedulePath, saveObj, "utf-8", () => {});
+                console.log("schedle sent");
+                res.send(parsedSchedule);
+            }
+        )
+            .on("error", (e) => {
+                console.error(`Get error: ${e.message}`);
+            })
+            .end();
+    } else {
+        res.send(currentSchedule);
     }
 };
+
+const parseSchedule = (html) => {
+    const $ = cheerio.load(html);
+    const ret = [];
+
+    const coachTables = $(".main table");
+
+    coachTables.each((i, coachTable) => {
+        const coach = $(coachTable).prev().text();
+        const coachTeams = $(coachTable).find("tr");
+        coachTeams.each((i, coachTeam) => {
+            const team = $(
+                $(coachTeam).find(
+                    "td > div:nth-child(2) > p:nth-child(1) > b"
+                )[0]
+            ).text();
+
+            if (!team || team.length === 0) return;
+
+            const obj = {
+                team,
+                coach,
+                html: $(coachTeam).html()
+            };
+            ret.push(obj);
+        });
+    });
+    return ret;
+};
+
+http.createServer(app).listen(UNSECURE_PORT, () => {
+    console.log("HTTP Server running on port " + UNSECURE_PORT);
+});
+
+console.log(process.argv[2]);
+if (process.argv[2] !== "no-secure") {
+    https
+        .createServer(
+            {
+                key: fs.readFileSync(
+                    path.join(__dirname, "certs", "www.goupstrong.com.key")
+                ),
+
+                cert: fs.readFileSync(
+                    path.join(__dirname, "certs", "www.goupstrong.com.crt")
+                ),
+
+                ca: fs.readFileSync(
+                    path.join(
+                        __dirname,
+                        "certs",
+                        "www.goupstrong.com.ca-bundle"
+                    )
+                )
+            },
+            app
+        )
+        .listen(SECURE_PORT, () => {
+            console.log("HTTPS Server running on port " + SECURE_PORT);
+        });
+}
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.get("/api/getTeamSchedule", getSchedule);
